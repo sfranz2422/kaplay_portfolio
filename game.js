10d0229facdf19c1d1366edc525bb6a2f5aca302
Steve <stephenfranz22@gmail.com>
2025-02-07 23:49:48 +0000

    end of friday

--- game.js
+++ game.js
@@ -1,28 +1,138 @@
+const textbox = document.getElementById("textbox");
+
 loadSprite("back", "/port4.png");
 loadSprite("desk", "/desk.png");
 
+// loadSprite("steve", "/steve.png");
+
+// Loading a multi-frame sprite
+loadSprite("steve", "/steve.png", {
+    // The image contains 9 frames layed out horizontally, slice it into individual frames
+    sliceX: 12,
+    // Define animations
+    anims: {
+        "run": {
+            from: 1,
+            to: 11,
+            speed: 15,
+            loop: true,
+        },
+        // This animation only has 1 frame
+        "idle": 0,
+    },
+});
+
+const SPEED = 80;
+setGravity(640);
 
 scene("game",() =>{
 
 
 add([
-      sprite("back"), // sprite() component makes it render as a sprite
-      // pos(120, 80), // pos() component gives it position, also enables movement
-      // rotate(0), // rotate() component gives it rotation
-      // anchor("center"), // anchor() component defines the pivot point (defaults to "topleft")
+      sprite("back"), 
+  pos(-80,48)
+   // body({ isStatic: true }),
   ]);
-
-
-  //   add([
-  //   text("coin dash"),
-  //   pos(24, 24),
-  // ])
 
   add([
       sprite("desk"), 
-    pos(40,75),
-    anchor("center")
+    pos(40,121),
+
+    area(),
+    anchor("center"),
+    "desk"
+
   ]);
+
+
+  add([
+      rect(960, .5),
+      area(),
+   
+      outline(.5),
+      pos(-80,142),
+      body({ isStatic: true }),
+  ]);
+  
+  const steve = add([
+      sprite("steve"), 
+    pos(40,75),
+    anchor("center"),
+    scale(.8),
+    area(),
+    body(),
+ "steve"
+  ]);
+  // steve.play("idle");
+
+  steve.onUpdate(() => {
+    setCamPos(steve.pos.x, height());
+  });
+
+  steve.onPhysicsResolve(() => {
+    // Set the viewport center to player.pos
+  setCamPos(steve.pos.x, height());
+  });
+
+  onKeyDown("right", () => {
+    // steve.play("run")
+    steve.move(SPEED, 0);
+      steve.flipX = false;
+    if (steve.isGrounded() && steve.curAnim() !== "run"){
+      steve.play("run")
+    }
+
+  });
+
+  onKeyDown("left", () => {
+    // steve.play("run")
+    steve.move(-SPEED, 0);
+      steve.flipX = true;
+    if (steve.isGrounded() && steve.curAnim() !== "run"){
+      steve.play("run")
+    }
+
+    onKeyRelease('left', ()=>{
+      if(steve.isGrounded() && !isKeyDown("left") && !isKeyDown("right")){
+        steve.play("idle")
+      }
+    })
+
+    onKeyRelease('right', ()=>{
+      if(steve.isGrounded() && !isKeyDown("left") && !isKeyDown("right")){
+        steve.play("idle")
+      }
+    })
+ 
+    steve.onCollide("desk", (desk) => {
+        // destroy(desk);
+      textbox.innerHTML = "<h1>Franz</h1>"
+
+      if (textbox.style.display === "none") {
+        textbox.style.display = "block";
+      } 
+      
+    });
+  });
+
+
+
+
+  onKeyPress("t", ()=>{
+    textbox.innerHTML = "<h1>Franz</h1>"
+
+    if (textbox.style.display === "none") {
+      textbox.style.display = "block";
+    } else {
+      textbox.style.display = "none";
+    }
+  })
+
+
+  steve.onCollideEnd("desk", (a) => {
+    textbox.style.display = "none";
+    
+  });
   
 // go("gameOver")
 
